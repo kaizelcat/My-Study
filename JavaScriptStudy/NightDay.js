af5d2d14f8bb0b315fe5cac266d551fdb13db2a1
kaizelcat <163068008+kaizelcat@users.noreply.github.com>
2025-02-15 22:21:53 +0900

    JS end

--- JavaScriptStudy/NightDay.js
+++ JavaScriptStudy/NightDay.js
@@ -1,20 +1,23 @@
 
 var Links = {
     SetColor: function(color) {
-        var i=0;
-        var alist = document.querySelectorAll('a');
-        while(i<alist.length){
-        alist[i].style.color = color;
-        i=i+1;
-        }
+        // var i=0;
+        // var alist = document.querySelectorAll('a');
+        // while(i<alist.length){
+        // alist[i].style.color = color;
+        // i=i+1;
+        // } // Ctrl + /
+        $('a').css('color',color); // $가 jquery에서 쓰고 2번이 매개변수 color
     }   
 }
 var Body = {
     SetColor : function (color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color',color);
     },
     SetBackGroundColor : function(color){
-        document.querySelector('body').style.backgroundColor = color;
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor',color);
     }
 }
 function nightDayHandler(self){
