452155a842638f726818d3224cf5cd050c1df4ee
kaizelcat <163068008+kaizelcat@users.noreply.github.com>
2025-02-15 22:00:39 +0900

    JS ++

--- JavaScriptStudy/NightDay.js
+++ JavaScriptStudy/NightDay.js
@@ -1,30 +1,35 @@
-function LinksSetColor(color){
-    var i=0;
-    var alist = document.querySelectorAll('a');
-    while(i<alist.length){
-    alist[i].style.color = color;
-    i=i+1;
+
+var Links = {
+    SetColor: function(color) {
+        var i=0;
+        var alist = document.querySelectorAll('a');
+        while(i<alist.length){
+        alist[i].style.color = color;
+        i=i+1;
+        }
+    }   
+}
+var Body = {
+    SetColor : function (color) {
+        document.querySelector('body').style.color = color;
+    },
+    SetBackGroundColor : function(color){
+        document.querySelector('body').style.backgroundColor = color;
     }
-}
-function BodySetColor(color){
-    document.querySelector('body').style.color = color;
-}
-function BodySetBackGroundColor(color){
-    document.querySelector('body').style.backgroundColor = color;
 }
 function nightDayHandler(self){
     var target = document.querySelector('body');
     if(self.value === 'night'){
-        BodySetBackGroundColor('black');
-        BodySetColor('white');
+        Body.SetBackGroundColor('black');
+        Body.SetColor('white');
         self.value = 'day';
-        LinksSetColor('red');
+        Links.SetColor('red');
     } 
     else {
-        BodySetBackGroundColor('white');
-        BodySetColor('black');
+        Body.SetBackGroundColor('white');
+        Body.SetColor('black');
         self.value = 'night';
-        LinksSetColor('blue')
+        Links.SetColor('blue')
     
     }
 }
